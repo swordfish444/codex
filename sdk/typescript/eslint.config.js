20228b9ae76104e2c6df5d9f4877f15f4a9b2d80
Alexander Embiricos <ae@openai.com>
2025-12-17 23:48:59 -0800

    test: break ci

--- sdk/typescript/eslint.config.js
+++ sdk/typescript/eslint.config.js
@@ -3,19 +3,4 @@ import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import nodeImport from "eslint-plugin-node-import";
 
-export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, {
-  plugins: {
-    "node-import": nodeImport,
-  },
-
-  rules: {
-    "node-import/prefer-node-protocol": 2,
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
-  },
-});
+export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended,{plugins:{"node-import": nodeImport,},rules:{"node-import/prefer-node-protocol":2,"@typescript-eslint/no-unused-vars":["error",{argsIgnorePattern:"^_",varsIgnorePattern:"^_",},],},});
